fe23c5ec5ea85b6286c477da64ab1d3ae802d8a9
KEmma6 <emma.chuyako@gmail.com>
2021-10-28 01:18:36 +0300

    first test

--- src/test/java/testSuites/RegistrationFormTest.java
+++ src/test/java/testSuites/RegistrationFormTest.java
@@ -1,8 +1,11 @@
 package testSuites;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
 
 import static com.codeborne.selenide.Selenide.*;
 
@@ -22,11 +25,18 @@ public class RegistrationFormTest {
         open("https://demoqa.com/automation-practice-form");
 
         //заполняем поля формы регистрации
-        $("#firstName").setValue("Биба");
-        $("id='lastName'").setValue("Боба");
-        $x("//input[id='lastName']").setValue("test@test.ru");
-        $("div.custom-radio > input[value = 'Female']").click();
+        $("#firstName").setValue("biba");
+        $("#lastName").setValue("boba");
+        $x("//input[@id='userEmail']").setValue("test@test.ru");
+        Selenide.executeJavaScript("document.querySelector('div.custom-radio > input[value = 'Female']',':before').click()");
+
+       // $("div.custom-radio > input[value = 'Female']").click();
         $("#userPhone").setValue("1234567899");
+        $("div#dateOfBirth input").setValue("27 Nov 1994");
+        $("div[class*='subjects-auto-complete__input']").selectOption("biology");
+        $("#hobbies-checkbox-3").click();
+        $("input#uploadPicture").setValue("C:\\Users\\kurga\\Desktop\\1540449964194466233.png");
+
 
 
 
