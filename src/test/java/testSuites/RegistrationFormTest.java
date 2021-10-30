8ca72b33ab5d6ee24b1fbc7084f98d04a5bcb088
KEmma6 <emma.chuyako@gmail.com>
2021-10-31 02:10:09 +0300

    first test for form registration on demoqa.com

--- src/test/java/testSuites/RegistrationFormTest.java
+++ src/test/java/testSuites/RegistrationFormTest.java
@@ -1,14 +1,13 @@
 package testSuites;
 
 import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.WebDriverRunner;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.interactions.Actions;
-
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class RegistrationFormTest {
@@ -17,11 +16,10 @@ public class RegistrationFormTest {
         static void doBeforeAllTest() {
 
         Configuration.startMaximized = true;
-
     }
 
     @Test
-    void assertRegistrationStudent() {
+    void checkStudentRegistration() {
 
         //открываем страницу с формой регистрации
         open("https://demoqa.com/automation-practice-form");
@@ -30,27 +28,33 @@ public class RegistrationFormTest {
         $("#firstName").setValue("biba");
         $("#lastName").setValue("boba");
         $x("//input[@id='userEmail']").setValue("test@test.ru");
-        $("div.custom-radio > input[value = 'Female']");
         Actions actions = new Actions(WebDriverRunner.getWebDriver());
         actions.moveToElement($("div.custom-radio > input[value = 'Female']")).click().build().perform();
-        $("#userNumber").sendKeys("7878787879");
-        $("div#dateOfBirth input").setValue("12 Nov 1996");
-//        WebDriver driver = new WebDriver();
-//        driver.Url = @"http://jqueryui.com/datepicker/";
-//        driver.SwitchTo().Frame(
-//                driver.FindElement(By.CssSelector("iframe.demo-frame")));
-        //SetDatepicker(driver, "#datepicker", "02/20/2002");
-        $("div[class*='subjects-auto-complete__input']").click();
-        $("div[class*='subjects-auto-complete__input']").sendKeys("b");
-        $("div[class*='react-select']").selectOptionContainingText("Biology");
-        $("#hobbies-checkbox-3").click();
+        $("#userNumber").setValue("7878787879");
+        $("div[class*='subjects-auto-complete__value']").click();
+        $("input#subjectsInput").setValue("Biology");
+        $("div#react-select-2-option-0").click();
+        actions.moveToElement($("#hobbies-checkbox-3")).click().build().perform();
         $("input#uploadPicture").setValue("C:\\Users\\kurga\\Desktop\\1540449964194466233.png");
+        $("textarea#currentAddress").setValue("44 Cedar Avenue");
+        $("div#state svg[class='css-19bqh2r']").scrollTo();
+        $("div#state svg[class='css-19bqh2r']").click();
+        $(byText("Uttar Pradesh")).click();
+        $("div#city svg[class='css-19bqh2r']").click();
+        $(byText("Agra")).click();
+        $("button#submit").click();
 
-
-
-
-
-
-
+        //Выполняем проверки
+        Assertions.assertTrue($x("//div[contains(text(), 'Thanks for submit')]").isDisplayed());
+        $("table[class*='table']").shouldHave(text("biba"));
+        $("table[class*='table']").shouldHave(text("boba"));
+        $("table[class*='table']").shouldHave(text("test@test.ru"));
+        $("table[class*='table']").shouldHave(text("Female"));
+        $("table[class*='table']").shouldHave(text("7878787879"));
+        $("table[class*='table']").shouldHave(text("Biology"));
+        $("table[class*='table']").shouldHave(text("1540449964194466233.png"));
+        $("table[class*='table']").shouldHave(text("44 Cedar Avenue"));
+        $("table[class*='table']").shouldHave(text("Uttar Pradesh"));
+        $("table[class*='table']").shouldHave(text("Agra"));
     }
 }
