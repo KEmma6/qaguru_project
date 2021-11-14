797564b77ad0dceffa9f9653d723cc6a4ace3e21
KEmma6 <emma.chuyako@gmail.com>
2021-11-14 22:02:55 +0300

    NEW COMMIT UNITED-5

--- src/main/java/FirstClass.java
+++ src/main/java/FirstClass.java
@@ -1,9 +1,10 @@
 public class FirstClass {
     public static void main(String[] args) {
-        Dracula dracula = new Dracula();
+        Dracula dracula =  Dracula.getInstance();
 
         dracula.printInformationOfDracula();
         dracula.biteHumans();
         dracula.isDraculaHungerAfterBite();
+        Dracula.resultsOfDraculasAttack();
     }
 }
